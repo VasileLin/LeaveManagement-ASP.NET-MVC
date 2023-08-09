38a4ad38d55677a05a336811f799c959d52ab684
VasileLin <vasile.linga1@gmail.com>
2023-08-09 15:30:39 +0300

    Added Database Tables and Migrations . End Implement Database Section

--- LeaveManagementWeb/Data/BaseEntity.cs
+++ LeaveManagementWeb/Data/BaseEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace LeaveManagementWeb.Data
 {
-    public partial class BaseEntity
+    public abstract class BaseEntity
     {
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
