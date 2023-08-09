499865dcd8846599fcc167b7b5ac8edea9ddf276
VasileLin <vasile.linga1@gmail.com>
2023-08-09 15:23:53 +0300

    Added Database Tables and Migrations.eend Implement Database Section

---
+++ LeaveManagementWeb/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementWeb.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ LeaveManagementWeb/Data/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementWeb.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfdays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+
+        public int EmployeeId { get; set; }
+
+    }
+}
